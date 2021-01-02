cb46f571c05b5690766361a2ae7c450ef6a5bb3b
Robert Lill <robert.lill@gmx.net>
2021-01-02 12:13:21 +0100

    dynamic port array length configuration

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # plug
 Raspberry Pi GPIO control with webservice
+
+
+
+sudo python3 plug.py 
+
+
+pip install gunicorn
+
+sudo gunicorn -w 4 -b 0.0.0.0:80 plug:app --preload  
+
+sudo nohup gunicorn -w 4 -b 0.0.0.0:80 plug:app --preload  &
+
+
+https://www.raspberry-pi-geek.de/ausgaben/rpg/2018/08/raspbian-im-read-only-modus/2/
+
+https://medium.com/@andreas.schallwig/how-to-make-your-raspberry-pi-file-system-read-only-raspbian-stretch-80c0f7be7353
+
+
+
--- plug.py
+++ plug.py
@@ -12,43 +12,41 @@ class PlugFlaskApp(Flask):
 
 app = PlugFlaskApp(__name__)
 
-sst = mp.Array(ctypes.c_bool, 8)
-
-led = LED(17)
-button = Button(2)
-
+sst = None
 config = None
 
-def toggle(port):
-	global led
+def toggle(si):
 	global sst
-	print("toggle ", port)
-	sst[port] = not sst[port]
-	if sst[port]:
-		led.on()
+	global config
+	sst[si] = not sst[si]
+	gpio = config['ports'][si]['gpio']
+	if sst[si]:
+		gpio.on()
 	else:
-		led.off()
+		gpio.off()
 
 def setup_app(app):
-	# All your initialization code
 	global config
+	global sst
         
-	sst[2] = True
+	with open("config.yaml", 'r') as stream:
+		try:
+			config = yaml.safe_load(stream)
+		except yaml.YAMLError as exc:
+			print(exc)
+			quit()
+	si = 0
+	for port in config['ports']:
+		port['gpio'] = LED(port['gpio_actor'])
 
-	if config == None:
-		button.when_pressed = partial(toggle, 0)
-		with open("config.yaml", 'r') as stream:
-			try:
-				config = yaml.safe_load(stream)
-				print(config)
-			except yaml.YAMLError as exc:
-				print(exc)
-				quit()
+		port['button'] = Button(port['gpio_button'])
+		port['button'].when_pressed = partial(toggle, si)
 
+		si = si + 1
+
+	sst = mp.Array(ctypes.c_bool, len(config['ports']))
 
 setup_app(app)
-
-
 
 @app.route('/api/v1/login', methods=['POST'])
 def api_login():
@@ -59,12 +57,14 @@ def api_login():
 @app.route('/api/v1/switch/<int:port>/<int:status>')
 def api_switch(port, status):
 	global sst
-	if port >= 0 and port <= 8:
+	global config
+	if port >= 0 and port < len(config['ports']):
 		sst[port] = (status != 0)
-	if sst[port]:
-		led.on()
-	else:
-		led.off()
+		gpio = config['ports'][port]['gpio']
+		if sst[port]:
+			gpio.on()
+		else:
+			gpio.off()
 	return 'OK'
 
 @app.route('/api/v1/status')
@@ -87,6 +87,5 @@ def index():
 	return '<a href="/switch">switch</a>'
 
 if __name__ == '__main__':
-	setup_app(app)
 	app.run()
 
